12e6792134fe46af5eaa79bb9096cc2a1bd31f0f
baasare <asarebernard98@gmail.com>
2018-07-22 14:53:52 +0000

    all files

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)      
+
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/result')
+def res():
+	return render_template('result.html')
+	
+ 
+if __name__ == '__main__':
+  app.run(debug=True)
